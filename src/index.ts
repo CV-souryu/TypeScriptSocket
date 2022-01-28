a7b9c6c5fa8356af56070f9883b2f217cc785d53
YueAo7 <leaoqi@outlook.com>
2022-01-29 02:18:14 +0800

    ✨ feat(ajv): ajv

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,54 @@
  * @FilePath: \noelle-core-v2\src\index.ts
  */
 import * as Net from "net"
-import Ajv, { Schema } from "ajv";
-const serve =  Net.createServer();
-const data:Schema={
-    type:""
+import Ajv ,{JTDSchemaType} from "ajv/dist/jtd"
+const ajv = new Ajv()
+type UserMSG={
+    msgType:"join",
+    nickName:string
+}|{
+    msgType:"leave",
+    nickName:string
+}|{
+    msgType:"move",
+    x:number,
+    y:number,
+    direction:string
 }
+type OutMSG = {
+    msgType:""
+}
+ const schema: JTDSchemaType<UserMSG> = {
+    "mapping":{
+        "join":{
+            "properties":{
+                nickName:{type:"string"}
+            }
+        },
+        "leave":{
+            "properties":{
+                nickName:{type:"string"}
+            }
+        },
+        "move":{
+            "properties":{
+                direction:{type:"string"},
+                x:{type:"int8"},
+                y:{type:"int8"}
+
+            }
+        }
+    },
+    "discriminator":"msgType"
+  } 
+  const serialize = ajv.compileSerializer(schema)
+
+  // parse will return MyData or undefined
+  const parse = ajv.compileParser(schema)
+
+
+const serve =  Net.createServer();
+
 serve.on("connection",(client)=>{
     Symbol()
     const ID = client.address()
